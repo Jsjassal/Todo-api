a1c50d51adcfd1f0ef216ab4efee3aa0fb61fc28
Japjeet Singh <japjeet.mail@gmail.com>
2016-05-12 20:18:03 -0400

    Convert email to lower case

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'japjeet'.,
+// 	age: 24
+// };
+
+// function updatePerson (obj) {
+// 	obj = {
+// 		name: 'japjeet',
+// 		age: 27
+// 	};
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+var grades = [15, 37];
+
+function addGrade(grades) {
+	grades.push(55);
+}
+
+addGrade(grades);
+console.log(grades);
